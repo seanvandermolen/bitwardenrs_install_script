6f675a1ba4b6dc1c8e76beadcb16c06d64332b92
urbancrawler <seanvandermolen@users.noreply.github.com>
2022-10-31 05:30:09 -0700

    Create kickstartinstall.sh
    Make install easier from root

---
+++ kickstartinstall.sh
@@ -0,0 +1,13 @@
+# If logged in as root add a user using these commands prior to install: 
+$ adduser vaultwarden
+$ usermod -a -G sudo vaultwarden
+# Switch to vaultwarden user (script won't run as root) 
+$ su vaultwarden
+# Change Directory to vaultwarden home 
+$ cd ~/
+# Download the install script from github 
+$ wget https://github.com/seanvandermolen/vaultwarden_install_script/raw/master/install.sh
+# Set Script as executable 
+$ chmod +x install.sh
+# Run script 
+$ ./install.sh
